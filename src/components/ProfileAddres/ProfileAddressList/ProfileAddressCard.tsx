610bace7fdc51eebea75ce3556771eca828ee3a4
Victor Le Roy <victorlrmatos@gmail.com>
2022-12-01 15:00:48 -0300

    feat: creating list and card to addresses

---
+++ src/components/ProfileAddres/ProfileAddressList/ProfileAddressCard.tsx
@@ -0,0 +1,22 @@
+import { FlatList, View } from "react-native";
+
+import { Address } from "../../../screens/ProfileAddress";
+
+import { styles } from "./styles";
+import { ProfileAddressCard } from "../ProfileAddressCard";
+
+interface Props {
+    addresses: Address[];
+}
+
+export const ProfileAddressList: React.FC<Props> = ({ addresses }) => {
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={addresses}
+                keyExtractor={(item) => String(item.id)}
+                renderItem={({ item }) => <ProfileAddressCard address={item} />}
+            />
+        </View>
+    );
+};
--- src/screens/ProfileAddress/ProfileAddress.tsx
+++ src/screens/ProfileAddress/ProfileAddress.tsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
-import { ProfileForm } from "../../components/ProfileForm";
-import { ScreenType } from "../../navigation/types";
-import { styles } from "./styles";
 
-type Props = ScreenType<"ProfileMenu">;
+import { api } from "../../service";
+import { useUser } from "../../context/user";
+import { ScreenType } from "../../navigation/types";
+
+import { styles } from "./styles";
+import { ProfileAddressList } from "../../components/ProfileAddres/ProfileAddressList";
+
+type Props = ScreenType<"ProfileAddres">;
+
+export interface Address {
+  id: number;
+  street: string;
+  number: number;
+  district: string;
+  cep: string;
+  complement: string;
+}
 
 export const ProfileAddress: React.FC<Props> = () => {
+
+  const [user] = useUser();
+  const [addresses, setAddresses] = React.useState<Address[]>();
+
+  useEffect(() => {
+    async function getAddress() {
+      const { data } = await api.get("/addresses", {
+        params: {
+          filter: {
+            userId: user?.id,
+          },
+        },
+      });
+      setAddresses(data);
+    }
+    getAddress();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text> ProfileAddress </Text>
+      <ProfileAddressList addresses={addresses}/>
     </View>
   );
 };
