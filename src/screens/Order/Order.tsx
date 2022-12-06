079547f8bdfd06a7107089ac8a1c9737bf96e175
Victor Le Roy <victorlrmatos@gmail.com>
2022-12-06 18:00:09 -0300

    feat: post order

--- src/screens/Order/Order.tsx
+++ src/screens/Order/Order.tsx
@@ -1,13 +1,18 @@
-import { useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
-import Toast from "react-native-toast-message";
 import { View } from "react-native";
+import Toast from "react-native-toast-message";
+import { useNavigation } from "@react-navigation/native";
+import { useAsyncStorage } from "@react-native-async-storage/async-storage";
+
+import { styles } from "./styles";
+import { api } from "../../service";
+import { NavigationProps, ScreenType } from "../../navigation/types";
+
 import { AddressCard } from "../../components/AddressCard";
 import { Button } from "../../components/Button";
 import { PaymentMethod } from "../../components/PaymentMethod";
 import { useUser } from "../../context/user";
-import { NavigationProps, ScreenType } from "../../navigation/types";
-import { styles } from "./styles";
+import axios from "axios";
 
 export interface OrderI {
   userId: string;
@@ -23,9 +28,11 @@ type Props = ScreenType<"Order">;
 export const Order: React.FC<Props> = () => {
   const [user] = useUser();
   const [order, setOrder] = useState<Partial<OrderI>>({
+    userId: user.id,
     address: { id: user.defaultAddress },
   });
   const navigation = useNavigation<NavigationProps>();
+  const { getItem } = useAsyncStorage("@cart");
 
   const changePaymentMethod = (paymentMethod: "pix" | "local") => {
     setOrder((prev) => {
@@ -33,15 +40,34 @@ export const Order: React.FC<Props> = () => {
     });
   };
 
+  const handlePostOrder = async (status: "processing" | "paid" | "local_receive" | "complete") => {
+    await api.post("/order", { ...order, status }).then((res) => {
+      console.log(res.data)
+    }).catch((err) => {
+      console.log(err)
+    });
+    // return orderData.data;
+  };
+
   const handleConfirmOrder = () => {
     // Todo create Pix Payment
     if (order.paymentMethod === "local") {
-      navigation.navigate("DashboardTabs");
-      Toast.show({
-        type: "success",
-        text1: "Sucesso",
-        text2: "Pedido concluido com sucesso",
-      });
+      try {
+        const orderData = handlePostOrder("local_receive");
+        console.log(orderData)
+        navigation.navigate("DashboardTabs");
+        Toast.show({
+          type: "success",
+          text1: "Sucesso",
+          text2: "Pedido concluido com sucesso",
+        });
+      } catch (error) {
+        Toast.show({
+          type: "error",
+          text1: "Erro",
+          text2: "Erro ao concluir pedido",
+        });
+      }
     }
   };
   return (
