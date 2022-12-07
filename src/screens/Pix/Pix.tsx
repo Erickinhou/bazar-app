6a26caf45287c32f01a46277428e37e114a7707e
Erickinhou <willianerick17@gmail.com>
2022-12-07 11:23:19 -0300

    feat: add fake pix

--- src/screens/Pix/Pix.tsx
+++ src/screens/Pix/Pix.tsx
@@ -1,10 +1,11 @@
 import { Text, View } from "react-native";
+import { PixPayment } from "../../components/PixPayment";
 import { styles } from "./styles";
 
 export const Pix: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text> Pix </Text>
+      <PixPayment />
     </View>
   );
 };
